d788db3b5e9c62170c96c54c25c3b9be1652fc29
Maged Eshak <magedeshak36@gmail.com>
2025-03-05 00:38:38 +0200

    first commit

---
+++ BOM.js
@@ -0,0 +1,16 @@
+console.log("-".repeat(20));
+console.log("2- BOM");
+console.log("-".repeat(20));
+
+console.log("-- التكليفات --");
+console.log("----------------------");
+
+console.log("-- التكليف 01 --");
+console.log("----------------------");
+
+let msg = prompt("Print Number From – To", "5-20");
+
+for (let i = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
